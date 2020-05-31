3f41ea905e65bc9eaf6cd0eaedf40c0afe4fd5c7
Dyss <florinraul97@yahoo.com>
2020-05-31 14:35:03 +0300

    integrated OPENCL and QT

--- src/ui/controller/controller.cpp
+++ src/ui/controller/controller.cpp
@@ -17,14 +17,14 @@ Controller::Controller() : QMainWindow(nullptr), controller(std::make_unique<Ui:
         controller->pathText->setText(dialog.selectedFiles().at(0));
     });
     connect(controller->openButton, &QPushButton::clicked, [this](){
-//        QPixmapConverter converter;
-//        auto pixels = QPixmap(controller->pathText->text());
-//        auto m_pixels = converter(pixels);
-//        auto m_bw_pixels = static_cast<BWImage>(m_pixels);
-//        auto m_bw_as_rgb = static_cast<RGBImage>(m_bw_pixels);
-//        auto bw_pixels = converter(m_bw_as_rgb);
-//        scene.addPixmap(bw_pixels);
-        OpenCLBase().main();
+        auto converter = QPixmapConverter();
+        auto pixels = QPixmap(controller->pathText->text());
+        auto m_pixels = converter(pixels);
+        auto m_bw_pixels = BWImage(m_pixels);
+        auto m_bw_as_rgb = RGBImage(m_bw_pixels);
+        auto bw_pixels = converter(m_bw_as_rgb);
+        scene.addPixmap(bw_pixels);
+//        OpenCLBase().main();
     });
     connect(controller->pathText, &QLineEdit::textChanged, [this](const QString& text){
         controller->openButton->setDisabled(text.isEmpty());
